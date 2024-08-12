7de214ed593ccb5109ad3617f94c97813e352c5b
Orbter <assouline39@gmail.com>
2024-08-12 22:24:32 +0300

    made the functionally of adding a skill and made it more efficient

--- src/a4Creator/a4Page.jsx
+++ src/a4Creator/a4Page.jsx
@@ -1,36 +1,23 @@
 import { useState, useContext } from 'react';
-
+import SkillA4 from './allA4 components/skillA4';
 import '../style/a4.css';
+
+import '../style/allA4/skillA4.css';
 import { ResumeContext } from '../formProvider';
 
 function A4() {
   const { objSeen, objNotSeen } = useContext(ResumeContext);
   const { skillData, setSkillData } = objSeen;
 
-  // Array.isArray(skillData)
   return (
     <div className="a4-page">
       <div className="a4">
-        <div className="a4-nav-bar"></div>
+        <div className="a4-nav-bar">
+          <SkillA4 skillData={skillData} />
+        </div>
         <div className="a4-main-content">
           <div className="a4-padding"></div>
-          <div className="a4-header">
-            <h2>Skills</h2>
-            <ul>
-              {Array.isArray(skillData)
-                ? skillData.map((skill, index) =>
-                    skill.skill !== '' ? (
-                      <li key={index}>
-                        {skill.skill} - Level: {skill.level} - Type:{' '}
-                        {skill.type}
-                      </li>
-                    ) : (
-                      ''
-                    )
-                  )
-                : setSkillData.skill}
-            </ul>
-          </div>
+          <div className="a4-header"></div>
         </div>
       </div>
     </div>
---
+++ src/a4Creator/allA4 components/skillA4.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+import '../../style/allA4/skillA4.css';
+
+function SkillA4({ skillData }) {
+  const [containsSoft, setContainsSoft] = useState(false);
+  const [containsHard, setContainsHard] = useState(false);
+
+  useEffect(() => {
+    const checkForSoft = skillData.some((skill) => skill.type === 'Soft');
+    const checkForHard = skillData.some((skill) => skill.type === 'Hard');
+    setContainsSoft(checkForSoft);
+    setContainsHard(checkForHard);
+  }, [skillData]);
+
+  return (
+    <div className="skill-list">
+      {containsHard && (
+        <div className="hard-skills-container">
+          <h3 className="hard-skills-headline">HARD SKILLS</h3>
+          <ul>
+            {Array.isArray(skillData) &&
+              skillData
+                .filter((skill) => skill.skill !== '' && skill.type === 'Hard')
+                .map((skill, index) => (
+                  <li key={index} className="skill-container">
+                    <div className="skill-name"> {skill.skill}</div>
+                    <div>
+                      {[1, 2, 3, 4, 5].map((index) => (
+                        <div
+                          className={
+                            'circle-skill' +
+                            (index <= skill.level ? ' active' : '')
+                          }
+                          key={index + ' HARD'}
+                        ></div>
+                      ))}
+                    </div>
+                  </li>
+                ))}
+          </ul>
+        </div>
+      )}
+
+      {containsSoft && (
+        <div className="soft-skills-container">
+          <h3 className="soft-skills-headLine">SOFT SKILLS</h3>
+          <ul>
+            {Array.isArray(skillData) &&
+              skillData
+                .filter((skill) => skill.skill !== '' && skill.type === 'Soft')
+                .map((skill, index) => (
+                  <li key={index} className="skill-container">
+                    <div className="skill-name"> {skill.skill}</div>
+                    <div>
+                      {[1, 2, 3, 4, 5].map((index) => (
+                        <div
+                          className={
+                            'circle-skill' +
+                            (index <= skill.level ? ' active' : '')
+                          }
+                          key={index + ' Soft'}
+                        ></div>
+                      ))}
+                    </div>
+                  </li>
+                ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+export default SkillA4;
